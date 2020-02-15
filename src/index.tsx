e8e11876f9d6179cc6a279f42dfb1615a7415195
Yuta Mori <moriyu54321@gmail.com>
2020-02-15 20:24:23 +0900

    add vertical example

--- src/index.tsx
+++ src/index.tsx
@@ -36,7 +36,7 @@ const Item: React.FC<{ style?: Object }> = props => {
   );
 };
 
-const App = () => {
+const App1 = () => {
   useEffect(() => {
     iNoBounce.enable();
   }, []);
@@ -92,7 +92,7 @@ const App = () => {
                 {index}
               </Item>
             ))}
-            itemMarginHorizontalPx={16}
+            itemMarginPx={16}
             snapToAlignment="center"
             onSnap={onSnap}
           />
@@ -102,4 +102,113 @@ const App = () => {
   );
 };
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+const hours = Array(24)
+  .fill(0)
+  .map((_, i) => i);
+const minutes = Array(60)
+  .fill(0)
+  .map((_, i) => i);
+
+const App2 = () => {
+  const [hour, setHour] = useState(0);
+  const [minute, setMinute] = useState(0);
+
+  return (
+    <div
+      style={{
+        fontFamily: "SF Pro Display, sans-serif",
+        boxSizing: "border-box",
+        height: "100vh",
+        backgroundColor: "#1C1C1E",
+        paddingTop: "56px"
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "#1C1C1E",
+          color: "#D1D1D3",
+          fontSize: "18px",
+          height: "224px",
+          overflow: "hidden",
+          display: "flex",
+          justifyContent: "center",
+          padding: "16px 0",
+          borderTop: "4px solid #2C2C2E",
+          borderBottom: "4px solid #2C2C2E",
+          position: "relative"
+        }}
+      >
+        <div style={{ width: "60px" }}>
+          <SnapScrollView
+            items={hours.map(hour => (
+              <div>{hour}</div>
+            ))}
+            itemMarginPx={8}
+            direction="vertical"
+            onSnap={({ focusedIndex }) => setHour(hours[focusedIndex])}
+          />
+        </div>
+        <div style={{ width: "60px" }}>
+          <SnapScrollView
+            items={minutes.map(minute => (
+              <div>
+                {minute.toString().length === 2 ? minute : `0${minute}`}
+              </div>
+            ))}
+            itemMarginPx={8}
+            direction="vertical"
+            onSnap={({ focusedIndex }) => setMinute(minutes[focusedIndex])}
+          />
+        </div>
+        <div
+          style={{
+            height: "1px",
+            width: "100%",
+            backgroundColor: "#49494B",
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            margin: "auto",
+            transform: "translateY(-18px)"
+          }}
+        />
+        <div
+          style={{
+            height: "1px",
+            width: "100%",
+            backgroundColor: "#49494B",
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            margin: "auto",
+            transform: "translateY(18px)"
+          }}
+        />
+      </div>
+
+      <div
+        style={{
+          color: "#fff",
+          textAlign: "center",
+          marginTop: "16px",
+          fontSize: "14px"
+        }}
+      >
+        Selected:{" "}
+        {`${hour}:${minute.toString().length === 2 ? minute : `0${minute}`}`}
+      </div>
+
+      <div style={{ color: "#fff", textAlign: "center", marginTop: "12vh" }}>
+        <h1 style={{ letterSpacing: "0.001em" }}>react-snap-scroll-view</h1>
+        <a
+          href="https://github.com/moriyuu/react-snap-scroll-view"
+          style={{ color: "#FE9E13" }}
+        >
+          moriyuu/react-snap-scroll-view
+        </a>
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App2 />, document.querySelector("#root"));
